5f47c146e7da8e993290bd335908a232c3c6c16a
tea2x <copperheart05@gmail.com>
2025-04-21 18:00:52 +0700

    Update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Remove the dist directory to ensure a clean build (optional)
+
 rm -rf dist
-# Build the package with wasm-pack
 wasm-pack build --out-dir dist --release --target web
-# Replace the default README with the npm-specific one
 cp README.npm.md dist/README.md
