c83b20b3dcb44a04af199502ad92c4e9e1f185f8
tea2x <copperheart05@gmail.com>
2026-01-15 10:31:52 +0700

    fix: adjust Scrypt log_n and update explanatory docs

--- src/constants.rs
+++ src/constants.rs
@@ -9,23 +9,30 @@ pub const MASTER_SEED_STORE: &str = "master_seed_store";
 pub const CHILD_KEYS_STORE: &str = "child_keys_store";
 pub const KDF_PATH_PREFIX: &str = "ckb/quantum-purse/sphincs-plus/";
 
-/// Scrypt’s original paper suggests N = 16384 (log_n = 14) for interactive logins via low-entropy passwords.
-/// QuantumPurse requires passwords of at least 20 characters together with the following scrypt param to protect the master seed in DB.
-/// 
 /// Given NIST new security post-quantum standards:
 /// 1) Key search on a block cipher with a 128-bit key (e.g. AES128)
 /// 3) Key search on a block cipher with a 192-bit key (e.g. AES192)
 /// 5) Key search on a block cipher with a 256-bit key (e.g. AES 256)
 /// 
-/// 0                  128       192       256
-/// |---------|---------|---------|---------|
-///                         ▲
-///         current setup: 152
+/// First protection layer: For a symetrical encryption practice, the first protection effort SHOULD be the responsibitlity of 
+/// the higher layer impelementation (Quantum Purse Wallet or any other system using this library) to ensure that the encrypted data
+/// is never exposed remotely/locally. It is also the responsibility of the end-users to always lock their device carefully.
 /// 
-/// The security with this setup falls between 1) and 3).
-/// Note that this is theoretical because user's password often has lower entropy than expected.
+/// Second protection layer: Should the first protection layer fail in any situation, the encryption itself stands as the last resistance
+/// against quantum attacks. It should be strong enough, so that breaking it requires comparable resouce to break the NIST category level 1), 3) and 5).
+/// 
+/// This library is aiming for level 1) minimum and let users decide if they want to go beyond that with longer passwords because
+/// longer passwords are hard to manage. Letting users choose a pass phrase (similar to bip39 but has clearer patterns) is a good practice
+/// but then if the passphrase is too long, it is unclear if we shouldlet users authenticate with the mnemonic seed directly.
+/// 
+/// At this second protection layer we have:
+///  - Minimum required 20-character passwords alone put us at ~128-bit of security in theory (less in reality because of human factors).
+///  - Scrypt with param {log_n = 17, r = 8, p = 1, len 32} make each effort to guess a password even harder for the attacker. 
+/// 
+/// The theoretical security for this setup, thus starts at level 1) and is not upper limited.
+/// 
 pub const ENC_SCRYPT: ScryptParam = ScryptParam {
-    log_n: 18,
+    log_n: 17,
     r: 8,
     p: 1,
     len: 32,
